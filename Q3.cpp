6dd4f40d56ea483d56656c3e8495dff81625db25
PanasheMasunzambwa <112359712+PanasheMasunzambwa@users.noreply.github.com>
2025-03-26 19:16:53 -0500

    Add files via upload

--- Q3.cpp
+++ Q3.cpp
@@ -8,13 +8,13 @@ int main(int argc, char *argv[]) {
     printf("hello world (pid:%d)\n", (int) getpid());
 
     int rc = fork();
-    if (rc < 0) { // fork failed; exit
+    if (rc < 0) { 
         fprintf(stderr, "fork failed\n");
         exit(1);
     } else if (rc == 0) { // child process
         printf("hello\n");
     } else { // parent process
-        usleep(100); // Small delay to let the child execute first
+        usleep(100); 
         printf("goodbye\n");
     }
 
